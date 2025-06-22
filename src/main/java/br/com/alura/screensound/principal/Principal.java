0bb22d9272208dfd78a05e7ca213d7ca0c135f1c
Danylo Baziewicz <danxbazi@gmail.com>
2025-06-22 20:31:51 -0300

    feat: implementa menu principal e cadastro de artistas com persistência via JPA

---
+++ src/main/java/br/com/alura/screensound/principal/Principal.java
@@ -0,0 +1,95 @@
+package br.com.alura.screensound.principal;
+
+import br.com.alura.screensound.model.Artista;
+import br.com.alura.screensound.model.TipoArtista;
+import br.com.alura.screensound.repository.ArtistaRepository;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    private final ArtistaRepository repositorio;
+    private Scanner scan = new Scanner(System.in);
+
+    public Principal(ArtistaRepository repositorio) {
+        this.repositorio = repositorio;
+    }
+
+    public void exibeMenu() {
+        var opcao = -1;
+
+        while (opcao!= 9) {
+            var menu = """
+                    *** Screen Sound Músicas ***
+                    
+                    1- Cadastrar artistas
+                    2- Cadastrar músicas
+                    3- Listar músicas
+                    4- Buscar músicas por artistas
+                    5- Pesquisar dados sobre um artista
+                    
+                    9 - Sair
+                    """;
+
+            System.out.println(menu);
+            opcao = scan.nextInt();
+            scan.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    cadastrarArtistas();
+                    break;
+                case 2:
+                    cadastrarMusicas();
+                    break;
+                case 3:
+                    listarMusicas();
+                    break;
+                case 4:
+                    buscarMusicasPorArtista();
+                    break;
+                case 5:
+                    pesquisarDadosDoArtista();
+                    break;
+                case 9:
+                    System.out.println("Encerrando a aplicação!");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        }
+    }
+
+    private void pesquisarDadosDoArtista() {
+
+    }
+
+    private void buscarMusicasPorArtista() {
+
+    }
+
+    private void listarMusicas() {
+
+    }
+
+    private void cadastrarMusicas() {
+
+    }
+
+    private void cadastrarArtistas() {
+        var cadastrarNovo = "s";
+        while (cadastrarNovo.equalsIgnoreCase("s")) {
+            System.out.println("Digite o nome do artista: ");
+            var nome = scan.nextLine();
+            System.out.println("Digite o tipo do artista: (solo, dupla ou banda)");
+            var tipo = scan.nextLine();
+            TipoArtista tipoArtista = TipoArtista.valueOf(tipo.toUpperCase());
+
+            var artista = new Artista(nome, tipoArtista);
+            repositorio.save(artista);
+            System.out.println("Artista cadastrado com sucesso!");
+            System.out.println("Cadastrar novo artista? (s/n)");
+            cadastrarNovo = scan.nextLine();
+        }
+    }
+}
